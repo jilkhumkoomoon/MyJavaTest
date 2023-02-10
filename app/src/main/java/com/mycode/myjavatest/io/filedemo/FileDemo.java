f8dfe0d4c1d1e1c6f2ec5299f3ef37c08bc3d197
peikun <peikun@coocaa.com>
2023-02-10 14:18:50 +0800

    File类常用API： exists()/delete()/createNewFile()/getName()/getAbsolutePath()/getParent() 的基本使用
    Change-Id: I144daf399280b3990af26e2794c7c6a774806531

---
+++ app/src/main/java/com/mycode/myjavatest/io/filedemo/FileDemo.java
@@ -0,0 +1,37 @@
+package com.mycode.myjavatest.io.filedemo;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileDemo {
+    public static void main(String[] args) {
+        File file = new File("E:\\work\\project\\MY_CODING_TEST\\java_coding\\files");
+        if(!file.exists()) {
+            file.mkdir();
+        } else {
+            file.delete();
+        }
+        System.out.println(file.isDirectory());
+
+        System.out.println(file.isFile());
+
+        File file1 = new File("E:\\work\\project\\MY_CODING_TEST\\java_coding\\files\\日记.txt");
+        if(!file1.exists()) {
+            try {
+                file1.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            file1.delete();
+        }
+
+        System.out.println(file); //E:\work\project\MY_CODING_TEST\java_coding\files  打印的式文件夹的绝对路径
+        System.out.println(file.getParentFile().getAbsolutePath()); //E:\work\project\MY_CODING_TEST\java_coding  当前文件夹节点的父节点的绝对路径
+        System.out.println(file1.getAbsolutePath()); //E:\work\project\MY_CODING_TEST\java_coding\files\日记.txt  文件的绝对路径
+        System.out.println(file.getName()); //files  路径最后一个文件节点的名称，不含路径
+        System.out.println(file1.getName()); //日记.txt 文件的名称，不含路径
+        System.out.println(file.getParent());  //E:\work\project\MY_CODING_TEST\java_coding  带路径  等效于file.getParentFile().getAbsolutePath()
+        System.out.println(file1.getParent());  //E:\work\project\MY_CODING_TEST\java_coding\files 带路径 等效于file1.getParentFile().getAbsolutePath()
+    }
+}
