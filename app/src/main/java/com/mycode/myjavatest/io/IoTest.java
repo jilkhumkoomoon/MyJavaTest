f2d0d610768cbe5a343b2c5ef764d7a88f344c38
moon <pei_kun1988@163.com>
2023-02-10 08:28:22 +0800

    首次提交

--- app/src/main/java/com/mycode/myjavatest/io/IoTest.java
+++ app/src/main/java/com/mycode/myjavatest/io/IoTest.java
@@ -3,10 +3,18 @@ package com.mycode.myjavatest.io;
 public class IoTest {
 
     public static void main(String[] args) {
-        String s = "1234";
+        String s = "慕课ABC";
+        byte[] bts = s.getBytes();
+        for(byte b : bts) {
+            /**
+             * toHexString()将字节转换为了int(32位)并以16进制的方式显示
+             * b & 0xff是将32位的前24位去掉，只保留后8位
+             * AS默认是采用UTF-8编码的：中文占3个字节，英文占1个字节
+             * */
+            System.out.print(Integer.toHexString(b & 0xff)+" ");
+        }
 
 
-
-        System.out.print("hello");
+//        /System.out.print("hello");
     }
 }
