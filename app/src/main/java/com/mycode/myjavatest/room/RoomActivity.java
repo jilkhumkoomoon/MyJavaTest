8a52fb4a9e22abd45e3c61558b862e03ec793966
peikun <peikun@coocaa.com>
2023-02-27 10:46:48 +0800

    Room demo运行正常，添加滚屏显示
    Change-Id: I1fdcab20e5457252901ab9496d9a6030f0f56961

--- app/src/main/java/com/mycode/myjavatest/room/RoomActivity.java
+++ app/src/main/java/com/mycode/myjavatest/room/RoomActivity.java
@@ -1,7 +1,94 @@
 package com.mycode.myjavatest.room;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.room.Room;
+
+import com.mycode.myjavatest.R;
+import com.mycode.myjavatest.room.database.Word;
+import com.mycode.myjavatest.room.database.WordDao;
+import com.mycode.myjavatest.room.database.WordDatabase;
+
+import java.util.List;
 
 public class RoomActivity extends AppCompatActivity {
+    WordDatabase wordDatabase;
+    WordDao wordDao;
+    TextView textView;
+    Button buttonInsert, buttonDelete, buttonUpdate, buttonClear;
+
+    LiveData<List<Word>> allWordsLive;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+        wordDatabase = Room.databaseBuilder(this, WordDatabase.class, "word_database").allowMainThreadQueries().build();
+        wordDao = wordDatabase.getWordDao();
+        textView = findViewById(R.id.textView);
+        allWordsLive = wordDao.getAllWords();
+
+        allWordsLive.observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                StringBuilder text = new StringBuilder();
+                for (Word word: words) {
+                    text.append(word.getId()).append("、").append(word.getWord()).append(":").append(word.getChinese()).append("\n");
+                }
+                textView.setText(text);
+            }
+        });
+        buttonInsert = findViewById(R.id.button1);
+        buttonDelete = findViewById(R.id.button2);
+        buttonUpdate = findViewById(R.id.button3);
+        buttonClear = findViewById(R.id.button4);
+
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Word word1 = new Word("Hello", "你好");
+                    Word word2 = new Word("World", "世界");
+                    wordDao.insertWords(word1, word2);
+                }
+            });
+
+        /**
+         * 删除id=30的Word
+         * */
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Word word = new Word("aa", "bb");
+                word.setId(30);
+                wordDao.deleWord(word);
+            }
+        });
+
+        /**
+         * 更新id=29的Word
+         * */
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Word word = new Word("Thanks", "谢谢");
+                word.setId(29);
+                wordDao.updateWord(word);
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wordDao.deleteAllWords();
+            }
+        });
+    }
 
 }
