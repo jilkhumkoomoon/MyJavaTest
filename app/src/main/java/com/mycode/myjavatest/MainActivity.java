5f333f947f407b4db47e0b5c190b545b92a988b5
jason.pei <jason.pei@spaceplus.tech>
2025-10-29 16:56:46 +0800

    test push

--- app/src/main/java/com/mycode/myjavatest/MainActivity.java
+++ app/src/main/java/com/mycode/myjavatest/MainActivity.java
@@ -3,6 +3,7 @@ package com.mycode.myjavatest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.mycode.myjavatest.mvp.SplashTimerPresenter;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("TAG", "onCreate: " );
 
     }
 
